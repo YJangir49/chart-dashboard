16a9693dbd96b7c88fb797e78d2b24558793d02c
Yogender Jangir <yogender.jangir@agrevolution.in>
2023-12-24 13:27:51 +0530

    [IMP] Add Boiler Plate Code

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-50 w-50 bg-red-500">
+      <h1>Welcome to dashboaard</h1>
+    </div>
+  );
+};
+
+export default App;
