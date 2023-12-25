f94d5e947048ef85cd8f5eeccf74948921bec459
Yogender Jangir <yogender.jangir@agrevolution.in>
2023-12-25 18:01:49 +0530

    [IMP] Add Gauze Chart

---
+++ src/components/charts/GauzeChart.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+
+const CustomGaugeChart = ({ gauzeOptions, data }) => {
+  const options = {
+    width: 320,
+    height: 320,
+    redFrom: 0,
+    redTo: 100,
+    yellowFrom: 100,
+    yellowTo: 200,
+    greenFrom: 200,
+    greenTo: 350,
+    minorTicks: 10,
+    max: 400,
+    min: -50,
+    animation: {
+      startup: true,
+      easing: "linear",
+      duration: 1500,
+    },
+    ...gauzeOptions,
+  };
+
+  return <Chart chartType="Gauge" data={data} options={options} />;
+};
+
+export default CustomGaugeChart;
