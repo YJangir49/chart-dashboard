c58cf43516853552e68cd57e30197d0b956ed567
Yogender Jangir <yogender.jangir@agrevolution.in>
2024-02-04 19:29:11 +0530

    [IMP] Pie With Recharts 2D

--- src/components/charts/CustomPie.js
+++ src/components/charts/CustomPie.js
@@ -1,36 +1,77 @@
-import React from "react";
-import { Chart } from "react-google-charts";
+import React, { PureComponent } from "react";
+import {
+  PieChart,
+  Pie,
+  Sector,
+  Cell,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
 
 const COLORS = ["green", "#e1ad01", "red"];
 
-const CustomPie = ({ data, title, unit }) => {
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
   return (
-    <Chart
-      chartType="PieChart"
-      data={[[title, unit], ...Object.entries(data)]}
-      width={"100%"}
-      height={"100%"}
-      options={{
-        chartArea: {
-          left: "15%",
-          top: 4,
-          width: "100%",
-          height: "88%",
-        },
-        title: "",
-        is3D: true,
-        backgroundColor: "none",
-        legend: {
-          textStyle: {
-            color: "white",
-          },
-        },
-        colors: COLORS,
-        slices: {
-          0: { offset: 0.3 },
-        },
-      }}
-    />
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+const CustomPie = ({ data }) => {
+  const pieData = data
+    ? Object.keys(data).map((key) => ({ name: key, value: data[key] }))
+    : [];
+  return (
+    <ResponsiveContainer width={"100%"} height={"100%"}>
+      <PieChart>
+        <Pie
+          data={pieData}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={"100%"}
+          fill="#8884d8"
+          dataKey="value"
+          paddingAngle={5}
+          legendType="rect"
+        >
+          {pieData.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={COLORS[index % COLORS.length]}
+              strokeWidth={0}
+            />
+          ))}
+        </Pie>
+        <Legend
+          align="right"
+          verticalAlign="top"
+          layout="vertical"
+          iconSize={10}
+        />
+      </PieChart>
+    </ResponsiveContainer>
   );
 };
 
