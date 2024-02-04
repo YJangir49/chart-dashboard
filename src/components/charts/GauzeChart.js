863c8928c1eba9e0ecc12d48716234e3431f3d2d
Yogender Jangir <yogender.jangir@agrevolution.in>
2024-02-04 18:19:30 +0530

    [UPD] Display Value Logic

--- src/components/charts/GauzeChart.js
+++ src/components/charts/GauzeChart.js
@@ -13,6 +13,12 @@ const CustomGaugeChart = ({
   width = "100%",
   height = "100%",
 }) => {
+  const needleValue =
+    value && value > maxValue
+      ? maxValue
+      : value && value < minValue
+      ? minValue
+      : value;
   return (
     <div
       style={{
@@ -26,19 +32,19 @@ const CustomGaugeChart = ({
         fluidWidth={true}
         minValue={minValue}
         maxValue={maxValue}
-        value={value}
+        value={needleValue}
         forceRender
         needleColor={needleColor}
-        // segments={50}
-        // startColor={startColor}
-        // endColor={endColor}
-        // segmentsStops
         needleHeightRatio={0.7}
         segmentColors={segmentColors}
         customSegmentStops={segmentsStops}
         paddingHorizontal={32}
         maxSegmentLabels={5}
         ringWidth={30}
+        labelFontSize={10}
+        valueTextFontSize={16}
+        currentValueText={`${value}`}
+        
       />
     </div>
   );
